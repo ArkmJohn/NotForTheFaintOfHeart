d11a8f20a037b59459381ec138ca7acd75f5700d
John Mark Adriano <j.adriano@saedubai.com>
2017-04-17 16:16:01 +0400

    OVR Controls

---
+++ NotForTheFaintOfHeart/Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Only be used on a test scene without VR
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour {
+
+    public float sensitivityModifier = 5.0f;
+    public float smoothModifier = 1f;
+
+    Vector2 look, smooth;
+    GameObject myCharacter;
+
+	// Use this for initialization
+	void Start () {
+        myCharacter = this.transform.parent.gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector2 mouseD = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        mouseD = Vector2.Scale(mouseD, new Vector2(sensitivityModifier * smoothModifier, sensitivityModifier * smoothModifier));
+        smooth.x = Mathf.Lerp(smooth.x, mouseD.x, 1f / smoothModifier);
+        smooth.y = Mathf.Lerp(smooth.y, mouseD.y, 1f / smoothModifier);
+        look += smooth;
+        look.y = Mathf.Clamp(look.y, -60, 60);
+
+        transform.localRotation = Quaternion.AngleAxis(-look.y, Vector3.right);
+        myCharacter.transform.localRotation = Quaternion.AngleAxis(look.x, myCharacter.transform.up);
+    }
+}
---
+++ NotForTheFaintOfHeart/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+public class CharacterScript : MonoBehaviour {
+
+    #region UtilityClasses
+
+    // Liked the Dropdown fo the RigidbodyFirstPersonController
+    [Serializable]
+    public class MovementSettings
+    {
+        [Tooltip("Movement Speed of the Character")]
+        public float movementSpeed = 1.0f;
+        [Tooltip("Modifier for moving sidewards")]
+        public float strafeSpeedModifier = 0.7f;
+        [Tooltip("Modifier for walking backward")]
+        public float backwardSpeedModifier = 0.5f;
+        public AnimationCurve SlopeCurveModifier = new AnimationCurve(new Keyframe(-90.0f, 1.0f), new Keyframe(0.0f, 1.0f), new Keyframe(90.0f, 0.0f));
+
+    }
+
+    #endregion
+
+    #region Public.Variables
+
+    public MovementSettings movementSettings = new MovementSettings();
+    [HideInInspector]
+    public float currentTargetSpeed = 1;
+
+    #endregion
+
+    #region Private.Variables
+
+    private Rigidbody myRB;
+    private Collider myCollider;
+    private Camera myCamera;
+    #endregion
+
+    #region UnityCallbacks
+
+    void Awake()
+    {
+        myRB = GetComponent<Rigidbody>();
+        myCollider = GetComponent<Collider>();
+        myCamera = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 myInput = ProcessInput();
+
+        if ((Mathf.Abs(myInput.x) > float.Epsilon || Mathf.Abs(myInput.y) > float.Epsilon))
+        {
+            Vector3 desiredVel = myCamera.transform.forward * myInput.y + myCamera.transform.right * myInput.x;
+            desiredVel.x = desiredVel.x * currentTargetSpeed;
+            desiredVel.z = desiredVel.z * currentTargetSpeed;
+            desiredVel.y = desiredVel.y * currentTargetSpeed;
+            //transform.Translate(desiredVel.x * Time.deltaTime, 0, desiredVel.y *Time.deltaTime);
+            if(myRB.velocity.sqrMagnitude < currentTargetSpeed * currentTargetSpeed)
+                myRB.AddForce(desiredVel * SlopeMultiplier(), ForceMode.Impulse);
+        }
+        myRB.drag = 5;
+        if (Mathf.Abs(myInput.x) < float.Epsilon && Mathf.Abs(myInput.y) < float.Epsilon && myRB.velocity.magnitude < 1f)
+        {
+            myRB.Sleep();
+        }
+    }
+
+    #endregion
+
+    #region Public.Functions
+
+    #endregion
+
+    #region Private.Functions
+
+    private Vector2 ProcessInput()
+    {
+        Vector2 input = new Vector2
+        {
+            x = CrossPlatformInputManager.GetAxis("Horizontal"),
+            y = CrossPlatformInputManager.GetAxis("Vertical")
+        };
+
+        if (input.x > 0 || input.x < 0)
+        {
+            currentTargetSpeed = (movementSettings.movementSpeed * movementSettings.strafeSpeedModifier);
+        }
+        if (input.y < 0)
+        {
+            currentTargetSpeed = (movementSettings.movementSpeed * movementSettings.strafeSpeedModifier);
+        }
+
+        return input;
+    }
+
+    private float SlopeMultiplier()
+    {
+        float angle = Vector3.Angle(new Vector3(0,1,0), Vector3.up);
+        return movementSettings.SlopeCurveModifier.Evaluate(angle);
+    }
+    #endregion
+}
