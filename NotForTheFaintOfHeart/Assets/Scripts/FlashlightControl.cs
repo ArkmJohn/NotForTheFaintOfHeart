c53b712be7223929657ad6f18928baf64e543dec
John Mark Adriano <j.adriano@saedubai.com>
2017-07-13 11:03:28 +0400

    Spoon

--- NotForTheFaintOfHeart/Assets/Scripts/FlashlightControl.cs
+++ NotForTheFaintOfHeart/Assets/Scripts/FlashlightControl.cs
@@ -8,30 +8,114 @@ public class FlashlightControl : MonoBehaviour {
     public GameObject flashlight;
     public CustomEvent flashOn, flashOff;
 
+    public bool isLightOn = false;
+    public float batteryChargeTime = 5.0f;
+    public float batteryUseTime = 10.0f;
+
+    public float batteryChargeCounter;
+    public float batteryUseCounter;
+
 	// Use this for initialization
 	void Start () {
         Cursor.visible = false;
+        batteryChargeCounter = 0;
+        batteryUseCounter = batteryUseTime;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetButtonDown("Fire1"))
         {
-            FlashLight();
+            UseFlashLight();
         }
+        BatteryLogic();
 	}
 
-    void FlashLight()
+    void UseFlashLight()
     {
+        if (!hasChargedBattery())
+            return;
+
         if (flashlight.GetComponent<Light>().isActiveAndEnabled)
         {
-            flashlight.GetComponent<Light>().enabled = false;
-            flashOff.PlayEvent(this.gameObject);
+            TurnOff();
         }
         else
         {
-            flashlight.GetComponent<Light>().enabled = true;
-            flashOn.PlayEvent(this.gameObject);
+            TurnOn();
         }
     }
+
+    void BatteryLogic()
+    {
+
+        if (!hasChargedBattery() && isLightOn)
+        {
+            TurnOff();
+        }
+
+        // Check if the battery has charge
+        if (!hasChargedBattery())
+        {
+            // If there is no charge charge the battery up
+            if (batteryChargeCounter < batteryChargeTime)
+                batteryChargeCounter += Time.deltaTime;
+            else
+                ChargeBattery();
+        }
+
+        
+        if (isLightOn)
+        {
+            // Drain the battery
+            BatteryDrain(Time.deltaTime);
+        }
+        else
+        {
+            // Charge the battery
+        }
+
+    }
+
+    void ChargeBattery()
+    {
+        if (hasChargedBattery())
+        {
+            batteryUseCounter = batteryUseTime;
+
+        }
+    }
+
+    void BatteryDrain(float timeCount)
+    {
+        if (hasChargedBattery())
+        {
+            batteryUseTime -= timeCount;
+        }
+    }
+
+    public void TurnOn()
+    {
+        flashlight.GetComponent<Light>().enabled = true;
+        flashOn.PlayEvent(this.gameObject);
+        isLightOn = true;
+    }
+
+    public void TurnOff()
+    {
+        flashlight.GetComponent<Light>().enabled = false;
+        flashOff.PlayEvent(this.gameObject);
+        isLightOn = false;
+    }
+
+    public bool hasChargedBattery()
+    {
+        if (batteryUseCounter < 0)
+        {
+            return false;
+        }
+        else
+            return true;
+    }
+
 }
