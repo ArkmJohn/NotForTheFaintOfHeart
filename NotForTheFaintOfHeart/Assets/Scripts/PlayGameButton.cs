c7e0780386140c83d4394dd09574e4fa9b540fce
ABDULRAHMAN\abdul <abdulrahmansoubhi@gmail.com>
2017-07-31 13:21:39 +0400

    a

---
+++ NotForTheFaintOfHeart/Assets/Scripts/PlayGameButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayGameButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+	void OnCollisionEnter(Collision collision)
+	{
+		//SceneManager.LoadScene (1,LoadSceneMode.Single);
+		Debug.Log ("Playing Game");
+	}
+}
