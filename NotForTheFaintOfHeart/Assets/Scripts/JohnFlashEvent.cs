0c62c579e92669d5d498e257d6b4ee3619ea9d76
ABDULRAHMAN\abdul <abdulrahmansoubhi@gmail.com>
2017-08-08 15:27:00 +0400

    B

---
+++ NotForTheFaintOfHeart/Assets/Scripts/JohnFlashEvent.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JohnFlashEvent : MonoBehaviour {
+
+	GameObject player;
+	public GameObject stillJohn;
+	bool istriggered;
+	public float timeToDestroy;
+
+	// Use this for initialization
+	void Start () 
+	{
+		stillJohn.SetActive (false);
+		istriggered = false;
+		player = FindObjectOfType<FlashlightControl> ().gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (istriggered) 
+		{
+			stillJohn.SetActive (true);
+			Destroy (stillJohn, timeToDestroy);
+		}
+	}
+
+	void OnTriggerEnter(Collider Col)
+	{
+		if (Col.gameObject.GetComponent<FlashlightControl> () != null) 
+		{
+			istriggered = true;
+		}
+	}
+}
